7dbb5f19974b688fc02bf901bdcf1d4d6ff7d018
Deshan Rashmika <deshanrashmika589@gmail.com>
2026-09-23 16:41:40 +0530

    Initial commit: Added displayMenu structure

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void displayMenu();
+
+
+int main()
+{
+    displayMenu();
+
+    return 0;
+
+}
+
+void displayMenu()
+{
+    printf("\n=====SMART VEHICAL RENTAL SYSTEM=====\n\n\n");
+    printf("1.Register New Rental\n");
+    printf("2.Display Rental Records\n");
+    printf("3.Display Fleet Status\n");
+    printf("4.Sort Rental by Oriority\n");
+    printf("5.View reports\n");
+    printf("6.Exit\n");
+}
